c90089e1d838cab0c4f2419b44dc66dc7d927d02
p00ja <30841009+poojasingh09@users.noreply.github.com>
2023-03-13 16:13:05 +0100

    Update 1_reseq_pipeline.sh

--- 1_reseq_pipeline.sh
+++ 1_reseq_pipeline.sh
@@ -1,8 +1,7 @@
 ##pipeline for analysing lamprologini genome-resequencing data 
 ##pooja.singh09@gmail.com
 
-
-for i in `cat bamfiles.txt | cut -d "." -f 1` ;  
+for i in `cat a.txt | cut -d "." -f 1` ;  
 
 do
 
@@ -35,12 +34,12 @@ rm $i.sorted.bam
 
 #######4 run fastQC quality control
 
-/usr/local/bioinf/bin/fastQC -o /netapp/home/singh/pooja_phd/data/taborsky_callipterus/ /netapp/home/singh/pooja_phd/data/taborsky_callipterus/$i.sorted.1.fq /netapp/home/singh/pooja_phd/data/taborsky_callipterus/$i.sorted.2.fq
+##/usr/local/bioinf/bin/fastQC -o /netapp/home/singh/pooja_phd/data/taborsky_callipterus/ /netapp/home/singh/pooja_phd/data/taborsky_callipterus/$i.sorted.1.fq /netapp/home/singh/pooja_phd/data/taborsky_callipterus/$i.sorted.2.fq
 
 
 ######5 trim first 10 wonky reads
 
-qsub -pe mpi 8 -l h_vmem=5G -b y -cwd -N gtrim -o ./gtrim.log -e ./gtrim.err /bin/sh trim.sh 
+#qsub -pe mpi 8 -l h_vmem=5G -b y -cwd -N gtrim -o ./gtrim.log -e ./gtrim.err /bin/sh trim.sh 
 #this is the script
 
 
@@ -60,18 +59,18 @@ rm $i.sorted.2.fq
 
 #######5.2 get qual stats after trimming
 
-/export/bio/fastx/latest/bin/fastx_quality_stats -Q33 -i $i.sorted.1.trimmed.fq -o $i.sorted.1.trimmed.fq.stats
+##/export/bio/fastx/latest/bin/fastx_quality_stats -Q33 -i $i.sorted.1.trimmed.fq -o $i.sorted.1.trimmed.fq.stats
 
-/export/bio/fastx/latest/bin/fastx_quality_stats -Q33 -i $i.sorted.2.trimmed.fq -o $i.sorted.2.trimmed.fq.stats
+##/export/bio/fastx/latest/bin/fastx_quality_stats -Q33 -i $i.sorted.2.trimmed.fq -o $i.sorted.2.trimmed.fq.stats
 
 
 
 
 ######5.3 plot nucleotide distribution along read after trimming
 
-/export/bio/fastx/latest/bin/fastx_nucleotide_distribution_graph.sh -i $i.sorted.1.trimmed.fq.stats -o $i.sorted.1.trimmed.fq.statsND.png -t "fastq_nucleotide_distribution"
+##/export/bio/fastx/latest/bin/fastx_nucleotide_distribution_graph.sh -i $i.sorted.1.trimmed.fq.stats -o $i.sorted.1.trimmed.fq.statsND.png -t "fastq_nucleotide_distribution"
 
-/export/bio/fastx/latest/bin/fastx_nucleotide_distribution_graph.sh -i $i.sorted.2.trimmed.fq.stats -o $i.sorted.2.trimmed.fq.statsND.png -t "fastq_nucleotide_distribution"
+##/export/bio/fastx/latest/bin/fastx_nucleotide_distribution_graph.sh -i $i.sorted.2.trimmed.fq.stats -o $i.sorted.2.trimmed.fq.statsND.png -t "fastq_nucleotide_distribution"
 
 
 
@@ -94,14 +93,15 @@ rm $i.sorted.2.fq
 ##########7 convert sam to bam 
 /usr/local/bioinf/bin/samtools view -bS -T /netapp/home/singh/pooja_phd/data/cichlid_reference_genomes/Neolamprologous_brichardi/N_brichardi_v1.assembly.fasta $i.Nb.bt2.sam > $i.Nb.bt2.bam
 
-rm $i.Nb.bt2.sam
+#rm $i.Nb.bt2.sam
 
 
 
 
 ######8 sort bam
+#qsub -pe mpi 8 -l h_vmem=5G -b y -cwd -N sortBAMfinal.$i -e sortBAMfinal.$i.err -o sortBAMfinal.$i.log 
 
-/usr/local/bioinf/bin/samtools sort $i.Nb.bt2.bam $i.Nb.bt2.sorted
+#/usr/local/bioinf/bin/samtools sort $i.Nb.bt2.bam $i.Nb.bt2.sorted
 
 
 
@@ -118,19 +118,131 @@ qsub -pe mpi 32 -b y -cwd -N RG -e rg.err -o rg.log /bin/sh read_group.sh
 
 #11 sort and index dedup bam
 
+for i in `ls *RG.bam | cut -d "." -f -4` ;
 
-sh dedup.sh
-
-
-#12 get coverage across bams
-
-sh cov.sh
-
-#13 samtools call SNPS and indels
+do
 
 
-sh varcall_all_Onil2.sh
+        java -jar /cl_tmp/singh/tools/picard-tools-1.119/MarkDuplicates.jar I=$i.bam O=$i.dedup.bam M=$i.dedup.metrics MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000
 
-echo "$i.finished" >> analysis_report.txt
+	/cl_tmp/singh/tools/samtools-1.3/samtools sort $i.dedup.bam -o $i.dedup.sorted 
+
+        #/cl_tmp/singh/tools/samtools-1.3/samtools index $i.dedup.sorted.bam
 
 done
+
+
+#12 indel realignment
+
+
+for i in `ls *RG.dedup.sorted.bam | cut -d "." -f -6` ;
+
+do
+
+
+#echo $i.bam
+
+#/cl_tmp/singh/tools/samtools-1.3/samtools index $i.bam
+
+
+/usr/bin/java -jar -Xmx10G /cl_tmp/singh/tools/gatk/GenomeAnalysisTK.jar -T RealignerTargetCreator -R /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/N_brichardi_v1.assembly.fasta -I $i.bam -o $i.intervals -log $i.intervals.log
+
+/usr/bin/java -jar -Xmx10G /cl_tmp/singh/tools/gatk/GenomeAnalysisTK.jar -T IndelRealigner -R /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/N_brichardi_v1.assembly.fasta -I $i.bam -targetIntervals $i.intervals -o $i.realinged.bam -log $i.realign.log
+
+
+#13 calcualte coverage
+
+for i in `cat cal`;
+
+do
+
+/cl_tmp/singh/tools/bedtools-2.17.0/bin/bedtools coverage -abam $i -b /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/bedtools_taborsky_project/N_brichardi_v1.assembly.windows.30kb.s36.txt > $i".covD.s36.30kb.bed"
+
+/cl_tmp/singh/tools/bedtools-2.17.0/bin/bedtools coverage -abam $i -b /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/bedtools_taborsky_project/N_brichardi_v1.assembly.windows.s23.30kb.15kbstep.txt > $i".covD.s23.30Kb.15kb.bed"
+
+done
+
+
+
+#14 GATK call SNPS and indels
+
+for i in `ls *realinged.bam | cut -d "." -f -7` ;
+
+do
+
+
+
+/usr/bin/java -jar -Xmx100G /cl_tmp/singh/tools/gatk/GenomeAnalysisTK.jar -T HaplotypeCaller -nct 16 -R /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/N_brichardi_v1.assembly.fasta -I $i.bam --genotyping_mode DISCOVERY --minPruning 3 -ERC GVCF -variant_index_type LINEAR -variant_index_parameter 128000 -stand_emit_conf 10 -stand_call_conf 30 -o $i.raw.vcf 
+
+done
+
+
+# filter SNPsc
+
+for i in `ls *raw.vcf | cut -d '.' -f -7` ;
+
+do 
+
+
+#echo $i
+
+
+/cl_tmp/singh/tools/vcftools_0.1.13/bin/vcftools --vcf $i.raw.vcf --out $i.filt --min-meanDP 8 --minQ 30 --remove-filtered-all --recode
+
+done
+
+## combine gVCF of filters vcfs
+
+/usr/bin/java -Xms10G -Xmx320G -jar /cl_tmp/singh/tools/gatk/GenomeAnalysisTK.jar -T CombineGVCFs -R /cl_tmp/singh/cichlid_reference_genomes/Neolamprologous_brichardi/N_brichardi_v1.assembly.fasta --variant gVCF_list2.list -o combined.realigned.filt.recode.vcf
+
+#15 make phlyogeny
+
+#bam2fasta
+for i in `ls *.Nb.bt2.RG.dedup.sorted.realinged.bam`; 
+
+do
+
+echo qsub -l h_vmem=100G -b y -cwd -N bam2fasta -e b2f.err -o b2f.log /cl_tmp/singh/tools/angsd/angsd -doFasta 2 -doCounts 1 -i $i -out $i".fasta"
+
+done
+
+#get genic fasta
+
+for i in `ls *.fasta | cut -f 1 -d "."`;
+
+do
+
+/cl_tmp/singh/tools/samtools-1.3/bin/samtools faidx $i".fasta"
+
+/cl_tmp/singh/tools/bedtools-2.25/bin/bedtools getfasta -name -fi $i".fasta" -bed genes.bed -fo $i".genes.fasta"
+
+done
+
+#align fasta
+for i in `ls *.fa | cut -f -4 -d "."`;
+
+do
+
+/cl_tmp/singh/tools/mafft-7.273-with-extensions/bin/mafft $i".fa" > $i".aligned.fa"
+
+/usr/bin/java -jar /cl_tmp/singh/tools/BMGE-1.12/BMGE.jar -i $i".aligned.fa" -t DNA -of $i".aligned.faa"
+
+/usr/bin/java -jar /cl_tmp/singh/tools/BMGE-1.12/BMGE.jar -i $i".aligned.fa" -t DNA -op $i".aligned.phylip"
+
+done
+
+
+## make raxml gene trees
+
+for i in `ls *.fas`;
+
+do
+
+#echo $i
+/cl_tmp/singh/tools/raxml/raxmlHPC-PTHREADS -T 10 -f a -s $i -p 589375 -x 12345 -N 100 -m GTRCAT -w /cl_tmp/singh/taborsky_callipterus/mapped_data/raxml_phylogeny/aligned/gene_trees/ -n $i
+
+done
+
+## make astral species tree
+
+
